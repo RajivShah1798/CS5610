91b7044a989f8384f4c7419f6e869e32870b5bf6
Rajiv Shah <shah.rajiv1702@gmail.com>
2024-02-22 23:54:09 -0500

    Updated styles for assignments

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -40,7 +40,7 @@ function Assignments() {
               <li className="list-group-item">
                 <FaEllipsisV className="me-2" />
                 <Link
-                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
+                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`} style={{color:"black"}}>{assignment.title}</Link>
                 <span className="float-end">
                   <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
               </li>))}
